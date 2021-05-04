34e60bce4388e6c019e4a577a2dfd5cee728f99d
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-05-04 10:31:41 +0100

    Added Moshi to parse JSON response

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,30 +17,37 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
 
-// TODO (04) Use the Moshi Builder to create a Moshi object with the KotlinJsonAdapterFactory
+// TODO (04) Use the Moshi Builder to create a Moshi object with the KotlinJsonAdapterFactory //DONE
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
 /**
  * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
  * object pointing to the desired URL
  */
 private val retrofit = Retrofit.Builder()
-        // TODO (05) Change the ConverterFactory to the MoshiConverterFactory with our Moshi Object
-        .addConverterFactory(ScalarsConverterFactory.create())
+        // TODO (05) Change the ConverterFactory to the MoshiConverterFactory with our Moshi Object //DONE
+        //.addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
 /**
  * A public interface that exposes the [getProperties] method
  */
-// TODO (06) Update the MarsApiService to return a List of MarsProperty Objects
+// TODO (06) Update the MarsApiService to return a List of MarsProperty Objects //DONE
 interface MarsApiService {
     /**
      * Returns a Retrofit callback that delivers a String
@@ -48,7 +55,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    fun getProperties(): Call<String>
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 /**
