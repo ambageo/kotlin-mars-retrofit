d13d980156f54e13305a1dfe3211baa9b4626222
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-05-04 10:05:30 +0100

    Added Retrofit to connect with the web service

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,40 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import java.util.stream.DoubleStream.builder
+
 private const val BASE_URL = "https://mars.udacity.com/"
 
-// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
-// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
-// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL //DONE
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String //DONE
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            /*
+            * Call<T> sends a request to a webserver and returns a response.*/
+            Call<String>
+}
+// TODO (04) Create a MarsApi public object to expose the Retrofit service to the rest of the app //DONE
+//  using Retrofit to implement the MarsApiService
+
+/*
+* object declares and creates a singleton. Since we only need one retrofit service instance (and it
+* is an expensive (network) call), we only create it once
+*/
+object MarsApi {
+    /*
+    * lazy initialization is the tactic of delaying the creation of the object until the time it is
+    * first needed
+    */
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
